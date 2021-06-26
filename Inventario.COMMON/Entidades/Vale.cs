a35c1d925fc81e58369bc3d830b6d45b0b60515c
Administrador <Administrador@WENDER-PC>
2021-06-25 21:36:04 -0400

    Capa COMMON creada y DAL en proceso
    De la DAL se quedo a medias el archivo de RepositorioDeEmpleados

---
+++ Inventario.COMMON/Entidades/Vale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventario.COMMON.Entidades
+{
+    public class Vale:Base
+    {
+        public DateTime FechaHoraSolicitud { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public DateTime? FechaEntregReal { get; set; }
+        public List<Material> MaterialesPrestados { get; set; }
+        public Empleado Solicitante { get; set; }
+        public Empleado EncargadoDeAlmcaen { get; set; }
+    }
+}
